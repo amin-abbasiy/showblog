e9400e9ec4b0de457c33816a05765e6ba3ad04f4
amin-abbasiy <rubyprogrameramin@gmail.com>
2018-10-01 08:09:05 +0330

    Authenticate

--- app/controllers/Users/sessions_controller.rb
+++ app/controllers/Users/sessions_controller.rb
@@ -4,14 +4,16 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+    flash[:danger] = "Invalid"
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+    flash[:danger] = "Invalid"
+  end
 
   # DELETE /resource/sign_out
   # def destroy
